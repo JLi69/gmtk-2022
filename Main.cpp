36be1256ab4f1c706e45b514de890332b0be4d3b
jli <coderguy50@gmail.com>
2022-07-16 09:02:16 -0500

    drew test rectangle

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 int main()
 {
@@ -14,9 +15,40 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(win);
+	//initialize glad
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	//Rectangle
+	float rect[] =
+	{
+		-1.0f, -1.0f,
+		-1.0f, 1.0f,
+		1.0f, 1.0f,
+		
+		1.0f, 1.0f,
+		1.0f, -1.0f,
+		-1.0f, -1.0f
+	};
+	unsigned int id;
+	glGenBuffers(1, &id);
+	glBindBuffer(GL_ARRAY_BUFFER, id);	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(rect), rect, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, false, 2 * sizeof(float), (void*)0);	
+	glEnableVertexAttribArray(0);
 
 	while(!glfwWindowShouldClose(win))
-	{
+	{	
+		//Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//test rectangle
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		//Glfw stuff
 		glfwPollEvents();
 		glfwSwapBuffers(win);
 	}
